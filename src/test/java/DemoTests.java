8f15e7dbec70a5ef4835b2b36d2c6443c62f1b33
oniet <oliver.nietlispach@gmail.com>
2016-11-01 16:55:36 +0100

    * added test for TSK sample * added dependencies for TSK in gradle

--- src/test/java/DemoTests.java
+++ src/test/java/DemoTests.java
@@ -1,5 +1,9 @@
 import ch.hsr.maloney.core.Demo;
 import org.junit.Test;
+import org.sleuthkit.datamodel.Examples.Sample;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import static org.junit.Assert.assertEquals;
 
@@ -9,12 +13,34 @@ import static org.junit.Assert.assertEquals;
 public class DemoTests {
 
     @Test
-    public void helloWorldTest(){
+    public void helloWorldTest() {
         assertEquals("Hello World!", Demo.helloWorld());
     }
 
     @Test
     public void tskTest() {
         assertEquals("Name.Something", Demo.testTsk());
+    }
+
+    @Test
+    public void tskTestSample() {
+        final String IMAGE_PATH = "..\\images\\autopsy-demo-disk.dd";
+
+        try{
+            Files.deleteIfExists(Paths.get(IMAGE_PATH + ".db"));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        System.loadLibrary("zlib");
+        System.loadLibrary("libewf");
+        System.loadLibrary("libvmdk");
+        System.loadLibrary("libvhdi");
+        System.loadLibrary("libtsk_jni");
+
+//        Demo.tskSample(path);
+        Sample.run(IMAGE_PATH);
+
+
     }
 }

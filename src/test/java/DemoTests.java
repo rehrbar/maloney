0d274c4b1fff5289fbb70ba501ecb29d08a07d7f
oniet <oliver.nietlispach@gmail.com>
2016-11-01 17:37:58 +0100

    removed integration test from demo test

--- src/test/java/DemoTests.java
+++ src/test/java/DemoTests.java
@@ -1,9 +1,5 @@
 import ch.hsr.maloney.core.Demo;
 import org.junit.Test;
-import org.sleuthkit.datamodel.Examples.Sample;
-
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 import static org.junit.Assert.assertEquals;
 
@@ -20,27 +16,5 @@ public class DemoTests {
     @Test
     public void tskTest() {
         assertEquals("Name.Something", Demo.testTsk());
-    }
-
-    @Test
-    public void tskTestSample() {
-        final String IMAGE_PATH = "..\\images\\autopsy-demo-disk.dd";
-
-        try{
-            Files.deleteIfExists(Paths.get(IMAGE_PATH + ".db"));
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-
-        System.loadLibrary("zlib");
-        System.loadLibrary("libewf");
-        System.loadLibrary("libvmdk");
-        System.loadLibrary("libvhdi");
-        System.loadLibrary("libtsk_jni");
-
-//        Demo.tskSample(path);
-        Sample.run(IMAGE_PATH);
-
-
     }
 }

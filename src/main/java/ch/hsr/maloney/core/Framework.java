1fd9407dd0d1992de826477d3b57a1a24053b522
oniet <oliver.nietlispach@gmail.com>
2016-11-01 16:10:46 +0100

    Added some first files

---
+++ src/main/java/ch/hsr/maloney/core/Framework.java
@@ -0,0 +1,20 @@
+package ch.hsr.maloney.core;
+
+/**
+ * Created by olive_000 on 25.10.2016.
+ */
+public class Framework {
+    private JobProcessor jobProcessor;
+    private Context context;
+
+    public Framework(JobProcessor jobProcessor, Context context){
+        this.jobProcessor = jobProcessor;
+        this.context = context;
+    }
+
+    private void initialize(){
+
+    }
+
+
+}
---
+++ src/main/java/ch/hsr/maloney/core/JobProcessor.java
@@ -0,0 +1,8 @@
+package ch.hsr.maloney.core;
+
+/**
+ * Created by olive_000 on 25.10.2016.
+ */
+public interface JobProcessor {
+
+}

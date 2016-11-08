06e6ccf30e7b4e150b1045b2a3249d51165d6b27
oniet <oliver.nietlispach@gmail.com>
2016-11-08 15:05:58 +0100

    Unnötiger Teil entfernt

--- src/main/java/ch/hsr/maloney/core/FrameworkController.java
+++ src/main/java/ch/hsr/maloney/core/FrameworkController.java
@@ -1,18 +1,10 @@
 package ch.hsr.maloney.core;
 
-import ch.hsr.maloney.processing.Job;
-
 /**
  * Created by olive_000 on 08.11.2016.
  */
 public class FrameworkController {
     void run(FrameworkConfiguration configuration, String imagePath){
 
-    }
-}
-
-class JobFactory {
-    public static Job createJob(String JobType){
-        return null;
     }
 }

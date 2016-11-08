3d138592c48dd980cc85adbd56a1184a051401d9
oniet <oliver.nietlispach@gmail.com>
2016-11-08 14:31:05 +0100

    Added FrameworkController and FrameworkConfiguration

---
+++ src/main/java/ch/hsr/maloney/core/FrameworkController.java
@@ -0,0 +1,18 @@
+package ch.hsr.maloney.core;
+
+import ch.hsr.maloney.processing.Job;
+
+/**
+ * Created by olive_000 on 08.11.2016.
+ */
+public class FrameworkController {
+    void run(FrameworkConfiguration configuration, String imagePath){
+
+    }
+}
+
+class JobFactory {
+    public static Job createJob(String JobType){
+        return null;
+    }
+}

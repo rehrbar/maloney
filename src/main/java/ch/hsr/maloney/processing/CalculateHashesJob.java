7005391a2c0148e6923e956717723f046467a43b
oniet <oliver.nietlispach@gmail.com>
2016-11-15 17:48:02 +0100

    Minor changes for easier readability

--- src/main/java/ch/hsr/maloney/processing/CalculateHashesJob.java
+++ src/main/java/ch/hsr/maloney/processing/CalculateHashesJob.java
@@ -1,5 +1,8 @@
 package ch.hsr.maloney.processing;
 
+import ch.hsr.maloney.storage.Artifact;
+import ch.hsr.maloney.storage.DataSource;
+import ch.hsr.maloney.storage.MetadataStore;
 import ch.hsr.maloney.util.Context;
 import ch.hsr.maloney.util.Event;
 import org.apache.logging.log4j.LogManager;
@@ -7,13 +10,12 @@ import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.security.DigestInputStream;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.UUID;
 
 /**
  * Created by olive_000 on 15.11.2016.
@@ -23,13 +25,16 @@ public class CalculateHashesJob implements Job {
     private final List<String> producedEvents = new LinkedList<>();
     private final Logger logger;
 
+    private final String NewFileEventName = "newFile";
     private final String MD5HashCalculatedEventName = "MD5HashCalculated";
     private final String SHA1HashCalculatedEventName = "SHA1HashCalculated";
+    private final String MD5HashType = "MD5Hash";
+    private final String SHA1HashType = "SHA1Hash";
 
     public CalculateHashesJob() {
         logger = LogManager.getLogger("CalculateHashesJob");
         //TODO check Event names, maybe track globally?
-        requiredEvents.add("newFile");
+        requiredEvents.add(NewFileEventName);
         producedEvents.add(MD5HashCalculatedEventName);
         producedEvents.add(SHA1HashCalculatedEventName);
     }
@@ -46,6 +51,10 @@ public class CalculateHashesJob implements Job {
 
     @Override
     public List<Event> run(Context ctx, Event evt) {
+        MetadataStore metadataStore = ctx.getMetadataStore();
+        DataSource dataSource = ctx.getDataSource();
+        UUID fileUuid = evt.getFileUuid();
+
         MessageDigest md5;
         MessageDigest sha1;
 
@@ -58,8 +67,7 @@ public class CalculateHashesJob implements Job {
             md5 = MessageDigest.getInstance("MD5");
             sha1 = MessageDigest.getInstance("SHA-1");
 
-            //TODO get actual file
-            InputStream is = Files.newInputStream(Paths.get("file.txt"));
+            InputStream is = dataSource.getFileStream(fileUuid);
 
             DigestInputStream md5dis = new DigestInputStream(is, md5);
             DigestInputStream sha1dis = new DigestInputStream(md5dis, sha1);
@@ -72,16 +80,21 @@ public class CalculateHashesJob implements Job {
             sha1digest = sha1.digest();
 
             is.close();
+
+            List<Artifact> artifacts = new LinkedList<>();
+            artifacts.add(new Artifact(getJobName(), md5digest, MD5HashType));
+            artifacts.add(new Artifact(getJobName(), sha1digest, SHA1HashType));
+
+            metadataStore.addArtifacts(fileUuid, artifacts);
+
+            events.add(new Event(MD5HashCalculatedEventName, getJobName(), fileUuid));
+            events.add(new Event(SHA1HashCalculatedEventName, getJobName(), fileUuid));
+
         } catch (IOException e) {
-            logger.error("Could not read file with UUID: " + evt.getFileUuid().toString(), e);
+            logger.error("Could not read file with UUID: " + fileUuid.toString(), e);
         } catch (NoSuchAlgorithmException e) {
             logger.error("Could not find specified Algorithm to calculate Hash", e);
         }
-        //TODO add Hashes as artifact to MetaDataStore
-        
-
-        events.add(new Event(MD5HashCalculatedEventName, this.getJobName(), evt.getFileUuid()));
-        events.add(new Event(SHA1HashCalculatedEventName, this.getJobName(), evt.getFileUuid()));
 
         return events;
     }

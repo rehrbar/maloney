4c632c734eadc758311cbaf5ced3c0839ec30a4c
Roman Ehrbar <r1ehrbar@hsr.ch>
2016-11-22 14:04:28 +0100

    Updates for Hash Tests
    Includes a new test to check if files with content also creates a correct hash.
    Applied refactorings from develop branch also to this implementations.

--- src/test/java/ch/hsr/maloney/processing/CalculateHashesJobTest.java
+++ src/test/java/ch/hsr/maloney/processing/CalculateHashesJobTest.java
@@ -1,37 +1,31 @@
 package ch.hsr.maloney.processing;
 
-import ch.hsr.maloney.storage.Artifact;
-import ch.hsr.maloney.storage.DataSource;
-import ch.hsr.maloney.storage.FileAttributes;
-import ch.hsr.maloney.storage.MetadataStore;
+import ch.hsr.maloney.storage.*;
 import ch.hsr.maloney.util.Context;
 import ch.hsr.maloney.util.Event;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-/**
- * Created by olive_000 on 15.11.2016.
- */
 public class CalculateHashesJobTest {
     private Path tempFilePath;
     private UUID tempFileUuid;
     private fakeMetaDataStore fakeMetaDataStore;
     private fakeDataSource fakeDataSource;
     private Context ctx;
+    public static final String ZERO_LENGTH_SHA_1_HASH = "da39a3ee5e6b4b0d3255bfef95601890afd80709";
+    public static final String ZERO_LENGTH_MD5_HASH = "d41d8cd98f00b204e9800998ecf8427e";
 
-    private class fakeMetaDataStore implements MetadataStore{
+    private class fakeMetaDataStore implements MetadataStore {
 
         Map<UUID, List<Artifact>> artifacts = new HashMap<>();
 
@@ -53,7 +47,7 @@ public class CalculateHashesJobTest {
         @Override
         public void addArtifact(UUID fileId, Artifact artifact) {
             List<Artifact> artifactList = artifacts.get(fileId);
-            if(artifactList == null){
+            if (artifactList == null) {
                 artifactList = new LinkedList<>();
             }
             artifactList.add(artifact);
@@ -62,122 +56,137 @@ public class CalculateHashesJobTest {
 
         @Override
         public void addArtifacts(UUID fileId, List<Artifact> artifacts) {
-            for (Artifact a:artifacts) {
+            for (Artifact a : artifacts) {
                 addArtifact(fileId, a);
             }
         }
 
-        public void clean(){
-            artifacts = new HashMap<>();
-        }
     }
 
-    private class fakeDataSource implements DataSource{
+    private class fakeDataSource implements DataSource {
 
-        Map<UUID, String> fileUuidToPath = new HashMap<>();
-
-        @Override
-        public void registerFileAttributes() {
-
-        }
+        Map<UUID, Path> fileUuidToPath = new HashMap<>();
 
         @Override
         public File getFile(UUID fileID) {
-            return new File(fileUuidToPath.get(fileID));
+            return fileUuidToPath.get(fileID).toFile();
         }
 
         @Override
         public FileInputStream getFileStream(UUID fileID) {
             try {
-                return new FileInputStream(new File(fileUuidToPath.get(fileID)));
+                return new FileInputStream(fileUuidToPath.get(fileID).toFile());
             } catch (FileNotFoundException e) {
                 throw new UnsupportedOperationException();
             }
         }
 
         @Override
-        public UUID addFile(String path, UUID parentId) {
+        public UUID addFile(UUID parentId, FileExtractor fileExtractor) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public Path getJobWorkingDir(Class job) {
+            throw new UnsupportedOperationException();
+        }
+
+        public UUID addFile(Path path) {
             UUID uuid = UUID.randomUUID();
             fileUuidToPath.put(uuid, path);
             return uuid;
         }
 
-        public void clean(){
-            fileUuidToPath = new HashMap<>();
-        }
     }
 
     @Before
-    public void prepare(){
+    public void prepare() {
         try {
-            tempFilePath = Files.createTempFile("CHJT-testfile-",null);
+            tempFilePath = Files.createTempFile("CHJT-testfile-", null);
         } catch (IOException e) {
             e.printStackTrace();
         }
         fakeMetaDataStore = new fakeMetaDataStore();
         fakeDataSource = new fakeDataSource();
         ctx = new Context(fakeMetaDataStore, null, fakeDataSource);
-        tempFileUuid = fakeDataSource.addFile(tempFilePath.toString(), null);
+        tempFileUuid = fakeDataSource.addFile(tempFilePath);
     }
 
     @After
-    public void cleanUp(){
-        try {
-            Files.deleteIfExists(tempFilePath);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public void cleanUp() {
+        fakeDataSource.fileUuidToPath.forEach((uuid, path) -> {
+            try {
+                Files.deleteIfExists(path);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
     }
 
     @Test
-    public void canRunTest(){
+    public void canRunTest() {
         Job job = new CalculateHashesJob();
 
         List<String> reqEvent = job.getRequiredEvents();
-        Event evt = new Event(reqEvent.get(reqEvent.size()-1), "Test", UUID.randomUUID());
+        Event evt = new Event(reqEvent.get(reqEvent.size() - 1), "Test", UUID.randomUUID());
 
         assertTrue(job.canRun(ctx, evt));
     }
 
     @Test
-    public void singleFileTest(){
+    public void singleFileTest() {
         Job job = new CalculateHashesJob();
-        Event evt = new Event("newFile","singleFileTest", tempFileUuid);
+        Event evt = new Event("newFile", "singleFileTest", tempFileUuid);
 
         job.run(ctx, evt);
 
         List<Artifact> createdArtifacts = fakeMetaDataStore.getArtifacts(tempFileUuid);
         assertTrue(createdArtifacts.size() == 2);
     }
-    @Test
-    public void checkMD5Hash(){
-        final String zeroLengthMD5Hash = "d41d8cd98f00b204e9800998ecf8427e";
 
+    @Test
+    public void checkMD5Hash() {
         Job job = new CalculateHashesJob();
-        Event evt = new Event("newFile","singleFileTest", tempFileUuid);
+        Event evt = new Event("newFile", "singleFileTest", tempFileUuid);
 
         job.run(ctx, evt);
 
         List<Artifact> createdArtifacts = fakeMetaDataStore.getArtifacts(tempFileUuid);
         createdArtifacts.stream().filter(a -> a.getType().equals("MD5Hash")).forEach(a -> {
-            assertEquals(zeroLengthMD5Hash, a.getValue().toString());
+            assertEquals(ZERO_LENGTH_MD5_HASH, a.getValue().toString());
         });
     }
 
     @Test
-    public void checkSHA1Hash(){
-        final String zeroLengthSHA1Hash = "da39a3ee5e6b4b0d3255bfef95601890afd80709";
-
-        Event evt;
-
+    public void checkSHA1Hash() {
         Job job = new CalculateHashesJob();
-        evt = new Event("newFile","singleFileTest", tempFileUuid);
+        Event evt = new Event("newFile", "singleFileTest", tempFileUuid);
 
         job.run(ctx, evt);
 
         List<Artifact> createdArtifacts = fakeMetaDataStore.getArtifacts(tempFileUuid);
         createdArtifacts.stream().filter(a -> a.getType().equals("SHA1Hash")).forEach(a -> {
-            assertEquals(zeroLengthSHA1Hash, a.getValue().toString());
+            assertEquals(ZERO_LENGTH_SHA_1_HASH, a.getValue().toString());
+        });
+    }
+
+    @Test
+    public void checkFileWithContentHash() throws IOException {
+        FileOutputStream fos = new FileOutputStream(tempFilePath.toFile(),false);
+        fos.write("Hello world!".getBytes());
+        fos.close();
+
+        Job job = new CalculateHashesJob();
+        Event evt = new Event("newFile", "singleFileTest", tempFileUuid);
+
+        job.run(ctx, evt);
+
+        List<Artifact> createdArtifacts = fakeMetaDataStore.getArtifacts(tempFileUuid);
+        createdArtifacts.stream().filter(a -> a.getType().equals("SHA1Hash")).forEach(a -> {
+            assertEquals("d3486ae9136e7856bc42212385ea797094475802", a.getValue().toString());
+        });
+        createdArtifacts.stream().filter(a -> a.getType().equals("MD5Hash")).forEach(a -> {
+            assertEquals("86fb269d190d2c85f6e0468ceca42a20", a.getValue().toString());
         });
     }
 }

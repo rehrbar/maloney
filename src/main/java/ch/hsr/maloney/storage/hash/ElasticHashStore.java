0a87b9a2b96d5bcb9cdfb3742e68a5fb4cbe72f1
Roman Ehrbar <r1ehrbar@hsr.ch>
2016-11-27 18:27:54 +0100

    HashStore in ElasticSearch
    Added the required methods to use Elasticsearch to store and query HashRecords.

---
+++ src/main/java/ch/hsr/maloney/storage/hash/ElasticHashStore.java
@@ -0,0 +1,110 @@
+package ch.hsr.maloney.storage.hash;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.elasticsearch.action.admin.indices.exists.indices.IndicesExistsResponse;
+import org.elasticsearch.action.delete.DeleteResponse;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.stream.Collectors;
+
+/**
+ * Hash store based on elastic search.
+ */
+public class ElasticHashStore implements HashStore {
+    final ObjectMapper mapper;
+    final Logger logger;
+    TransportClient client;
+    static final String INDEX_NAME = "hashes";
+    static final String HASHRECORD_TYPE = "hashrecord";
+
+    /**
+     * Creates a new Instance of MetadataStore with ElasticSearch as backend.
+     *
+     * @throws UnknownHostException
+     */
+    public ElasticHashStore() throws UnknownHostException {
+        this.logger = LogManager.getLogger();
+        // TODO pass configuration to transportclient for cluster name and node.
+        client = new PreBuiltTransportClient(Settings.EMPTY)
+                .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("localhost"), 9300));
+        logger.info("Connected nodes: {}", String.join(", ", client.connectedNodes()
+                .stream().map(node -> node.getName()).collect(Collectors.toList())));
+        updateMapping(false);
+        mapper = new ObjectMapper();
+        mapper.configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true);
+    }
+
+    void updateMapping(boolean force) {
+        // https://www.elastic.co/guide/en/elasticsearch/guide/current/_finding_exact_values.html
+        boolean wasCreated = false;
+        IndicesExistsResponse existsResponse = client.admin().indices().prepareExists(INDEX_NAME).get();
+        if (!existsResponse.isExists()) {
+            client.admin().indices().prepareCreate(INDEX_NAME).get();
+        }
+        if (wasCreated || force) {
+            // Index has to be created to work.
+
+            // TODO add some mapping information.
+        }
+    }
+    @Override
+    public String addHashRecord(HashRecord record) {
+        try {
+            // TODO test if everything is serialized correctly.
+            byte[] data = mapper.writeValueAsBytes(record);
+            IndexResponse indexResponse = client.prepareIndex(INDEX_NAME, HASHRECORD_TYPE).setSource(data).get();
+            logger.debug("Indexed HashRecord with id: {}", indexResponse.getId());
+            return indexResponse.getId();
+        } catch (JsonProcessingException e) {
+            logger.error("Could not serialize HashRecord.", e);
+        }
+        return null;
+    }
+
+    @Override
+    public void removeHashRecord(String id) {
+        DeleteResponse deleteResponse = client.prepareDelete(INDEX_NAME, HASHRECORD_TYPE, id).get();
+        logger.debug("Performed delete action for HashRecord with result {}", deleteResponse.status());
+    }
+
+    @Override
+    public HashRecord getHashRecord(String id) {
+        GetResponse response = client.prepareGet(INDEX_NAME, HASHRECORD_TYPE, id).get();
+        try {
+            return mapper.readValue(response.getSourceAsBytes(), HashRecord.class);
+        } catch (IOException e) {
+            logger.error("Could not parse HashRecord retrieved from elasticsearch.", e);
+        }
+        return null;
+    }
+
+    @Override
+    public HashRecord findHash(String hashValue) {
+        // TODO check if this query is correct.
+        SearchResponse searchResponse = client.prepareSearch(INDEX_NAME)
+                .setTypes(HASHRECORD_TYPE)
+                .setQuery(QueryBuilders.termQuery("hashes.value", hashValue))
+                .setFrom(0).setSize(1).get();
+            return getHashRecord(searchResponse.getHits().getAt(0).id());
+    }
+
+    @Override
+    public HashRecord findHash(String hashValue, HashType type) {
+        // TODO improve search to pass type to speed up search.
+        return findHash(hashValue);
+    }
+}

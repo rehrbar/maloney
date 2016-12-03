9fead8c0854a446d7c0d323135e2e585a62828dd
Roman Ehrbar <r1ehrbar@hsr.ch>
2016-12-03 18:10:32 +0100

    Improved Dependency Workaround
    Improved the workaround for libraries not built into sleuthkit jni.

--- maloney-cli/src/main/java/ch/hsr/maloney/maloney_cli/Start.java
+++ maloney-cli/src/main/java/ch/hsr/maloney/maloney_cli/Start.java
@@ -3,15 +3,20 @@ package ch.hsr.maloney.maloney_cli;
 import ch.hsr.maloney.core.FrameworkController;
 
 /**
- * Created by r1ehrbar on 17.10.2016.
+ * Entry point for the command line interface of Maloney.
  */
 public class Start {
     public static void main(String[] args) {
-        System.loadLibrary("zlib");
-        System.loadLibrary("libewf");
-        System.loadLibrary("libvmdk");
-        System.loadLibrary("libvhdi");
-        System.loadLibrary("libtsk_jni");
+        // Required override due to building problems of TSK on Windows 7 with .NET Framework >= 4.5 or newer Windows versions.
+        // All libraries need to be inside java.library.path
+        // Example: -Djava.library.path="C:\libs"
+        if(System.getProperty("os.name").toLowerCase().contains("windows")) {
+            System.loadLibrary("zlib");
+            System.loadLibrary("libewf");
+            System.loadLibrary("libvmdk");
+            System.loadLibrary("libvhdi");
+            System.loadLibrary("libtsk_jni");
+        }
 
         final String IMAGE_PATH = args[0];
         System.out.println("Starting framework with image in: " + IMAGE_PATH);

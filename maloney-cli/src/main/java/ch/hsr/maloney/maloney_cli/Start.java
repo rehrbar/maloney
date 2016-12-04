76e379cc7ece106d4086505a23b5a7141f527e1f
Roman Ehrbar <r1ehrbar@hsr.ch>
2016-12-04 18:02:25 +0100

    Easy-to-use CLI
    Added apache commons cli to provide fast command line parsing and help generation.

--- maloney-cli/src/main/java/ch/hsr/maloney/maloney_cli/Start.java
+++ maloney-cli/src/main/java/ch/hsr/maloney/maloney_cli/Start.java
@@ -2,6 +2,14 @@ package ch.hsr.maloney.maloney_cli;
 
 import ch.hsr.maloney.core.FrameworkConfiguration;
 import ch.hsr.maloney.core.FrameworkController;
+import org.apache.commons.cli.*;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.LoggerContext;
+import org.apache.logging.log4j.core.appender.ConsoleAppender;
+import org.apache.logging.log4j.core.config.Configuration;
+import org.apache.logging.log4j.core.config.LoggerConfig;
 
 import java.io.IOException;
 
@@ -9,11 +17,14 @@ import java.io.IOException;
  * Entry point for the command line interface of Maloney.
  */
 public class Start {
+
+    private static Options options;
+
     public static void main(String[] args) {
         // Required override due to building problems of TSK on Windows 7 with .NET Framework >= 4.5 or newer Windows versions.
         // All libraries need to be inside java.library.path
         // Example: -Djava.library.path="C:\libs"
-        if(System.getProperty("os.name").toLowerCase().contains("windows")) {
+        if (System.getProperty("os.name").toLowerCase().contains("windows")) {
             System.loadLibrary("zlib");
             System.loadLibrary("libewf");
             System.loadLibrary("libvmdk");
@@ -21,17 +32,66 @@ public class Start {
             System.loadLibrary("libtsk_jni");
         }
         // TODO convert args to hash map to make them easier available
-        // TODO allow overrides of FrameworkConfiguration
-        // TODO pass working directory
-        // TODO pass job configurations
-        // TODO pass jobs to execute
-        // TODO add generator for example configuration
+
+        CommandLineParser parser = new DefaultParser();
+
+        options = new Options();
+        options.addOption("v", "verbose", false, "enables verbose output");
+        options.addOption("c", "configuration", true, "configuration to load");
+        options.addOption("sc", "save-configuration", true, "saves an example configuration");
+        options.addOption("h", "help", false, "prints this help");
+
         try {
-            FrameworkConfiguration config = FrameworkConfiguration.loadFromFile(args[0]);
-            FrameworkController.run(config);
+            CommandLine line = parser.parse(options, args);
+
+            if (line.hasOption("h")) {
+                printHelp();
+                return;
+            }
+
+            // Preparing the logger.
+            if(line.hasOption("v")){
+                // TODO test if this is working properly.
+                LoggerContext ctx = (LoggerContext) LogManager.getContext(false);
+                Configuration config = ctx.getConfiguration();
+                LoggerConfig loggerConfig = config.getLoggerConfig(LogManager.ROOT_LOGGER_NAME);
+                loggerConfig.setLevel(Level.DEBUG);
+                ctx.updateLoggers();
+            }
+            Logger logger = LogManager.getLogger();
+            logger.info("Hello world");
+            logger.debug("now in debugging mode");
+
+            if (line.hasOption("sc")) {
+                // TODO add generator for example configuration
+                new FrameworkConfiguration().saveToFile(line.getOptionValue("sc"));
+                return;
+            }
+
+            if (line.hasOption("c")) {
+                // TODO allow overrides of FrameworkConfiguration
+                // TODO pass working directory
+                // TODO pass job configurations
+                // TODO pass jobs to execute
+                FrameworkConfiguration frameworkConfiguration = FrameworkConfiguration.loadFromFile(line.getOptionValue("c", "./configuration.json"));
+                FrameworkController.run(frameworkConfiguration);
+                return;
+            }
+
+            if(line.getArgList().size()>0) {
+                // TODO rplace this old behaviour with an image file through the configurable version.
+                FrameworkController.run(line.getArgs()[0]);
+            }
         } catch (IOException e) {
             e.printStackTrace();
-            // TODO print example usage
+        } catch (ParseException e) {
+            System.out.println("Could not parse arguments: " + e.getMessage());
+            printHelp();
         }
+    }
+
+    private static void printHelp() {
+        HelpFormatter formatter = new HelpFormatter();
+        formatter.printHelp("maloney", options, true);
     }
 }

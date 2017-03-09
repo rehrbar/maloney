d447e355f2a07ffc37a304c15176ee073da93faa
oniet <oliver.nietlispach@gmail.com>
2017-03-09 16:31:03 +0100

    added multithreaded job processor. tests pending

---
+++ src/main/java/ch/hsr/maloney/processing/MultithreadedJobProcessor.java
@@ -0,0 +1,7 @@
+package ch.hsr.maloney.processing;
+
+/**
+ * Created by olive_000 on 09.03.2017.
+ */
+public class MultithreadedJobProcessor {
+}
---
+++ src/test/java/ch/hsr/maloney/processing/MultithreadedJobProcessorTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by olive_000 on 09.03.2017.
+ */
+public class MultithreadedJobProcessorTest {
+
+}

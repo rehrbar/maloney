8617691417575b4476ba6dc1810ef57497fdbae2
Oliver Nietlispach <oliver.nietlispach@gmail.com>
2017-03-23 16:38:25 +0100

    Multithreading Finished
    See title

--- src/main/java/ch/hsr/maloney/processing/MultithreadedJobProcessor.java
+++ src/main/java/ch/hsr/maloney/processing/MultithreadedJobProcessor.java
@@ -6,12 +6,11 @@ import ch.hsr.maloney.util.Tuple;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
-import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.Semaphore;
-import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author oniet
@@ -25,20 +24,17 @@ public class MultithreadedJobProcessor extends JobProcessor {
     private final Logger logger;
     private final Queue<JobExecution> readyJobs; //TODO replace with better Queueing structure (persistent)
 
+    private boolean isStarted =false;
     private Context ctx;
 
-    private Thread controllerThread;
-    private boolean stopProcessing;
     private ForkJoinPool pool;
-    Semaphore semaphore = new Semaphore(CONCURRENTJOBS);
+    private Semaphore semaphore = new Semaphore(CONCURRENTJOBS);
 
     public MultithreadedJobProcessor(Context ctx) {
         logger = LogManager.getLogger();
         this.ctx = ctx;
-        readyJobs = new ConcurrentLinkedQueue<>();
+        readyJobs = new LinkedList<>();
         pool = new ForkJoinPool();
-
-        stopProcessing = false;
     }
 
     @Override
@@ -49,65 +45,56 @@ public class MultithreadedJobProcessor extends JobProcessor {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        controllerThread = new Thread(() -> {
-            logger.debug("Started controllerThread");
+        logger.debug("Started controllerThread");
 
-            while (!stopProcessing) {
-                while (!readyJobs.isEmpty() && !stopProcessing) {
-                    JobExecution jobExecution = readyJobs.poll();
-                    Job job = jobExecution.getJob();
-                    Event evt = jobExecution.getEvent();
+        isStarted = true;
 
-                    if (job.canRun(ctx, evt)) {
-
-                        try {
-                            logger.debug("Trying to aqcuire token");
-                            semaphore.acquire();
-                            logger.debug("Acquired token");
-                            pool.submit(() -> {
-                                List<Event> result = job.run(ctx, evt);
-                                if (result != null && !result.isEmpty()) {
-                                    setChanged();
-                                    notifyObservers(result);
-                                }
-                                semaphore.release();
-                                logger.debug("Released token");
-                            });
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }
-            }
-            logger.debug("Nothing more to process or processing canceled");
-        });
-
-        controllerThread.start();
+        while (!readyJobs.isEmpty()) {
+            JobExecution jobExecution = readyJobs.poll();
+            putInPool(jobExecution);
+        }
+        logger.debug("Nothing more to process or processing canceled");
         semaphore.release();
+    }
+
+    private void putInPool(JobExecution jobExecution) {
+        Job job = jobExecution.getJob();
+        Event evt = jobExecution.getEvent();
+
+        if (job.canRun(ctx, evt)) {
+
+            try {
+                logger.debug("Trying to aqcuire token");
+                semaphore.acquire();
+                logger.debug("Acquired token");
+                pool.submit(() -> {
+                    List<Event> result = job.run(ctx, evt);
+                    if (result != null && !result.isEmpty()) {
+                        setChanged();
+                        notifyObservers(result);
+                    }
+                    semaphore.release();
+                    logger.debug("Released token");
+                });
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
     public void stop() {
-        if (controllerThread.getState() != Thread.State.TERMINATED) {
-            logger.debug("Terminating JobProcessor...");
-            stopProcessing = true;
-            try {
-                controllerThread.join();
-            } catch (InterruptedException e) {
-                logger.fatal("Could not stop JobProcessor");
-            }
-        } else {
-            logger.debug("JobProcessor was already terminated");
-        }
+        pool.shutdown();
     }
 
     @Override
     public void enqueue(Job job, Event event) {
         logger.debug("Enqueued '{}' to '{}'", event.getName(), job.getJobName());
-        //TODO enqueue directly in pool
-        synchronized (readyJobs) {
-            readyJobs.add(new JobExecution(job, event));
-            readyJobs.notifyAll();
+        JobExecution jobExecution = new JobExecution(job, event);
+        if(isStarted && !pool.isShutdown()){
+            putInPool(jobExecution);
+        } else {
+            readyJobs.add(jobExecution);
         }
     }
 

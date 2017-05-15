e87dc04b2246f17291915b87b639c3ac1d256442
Oliver Nietlispach <oliver.nietlispach@gmail.com>
2017-05-15 21:50:38 +0200

    changed method signature of Category

--- maloney/src/main/java/ch/hsr/maloney/util/Category.java
+++ maloney/src/main/java/ch/hsr/maloney/util/Category.java
@@ -3,8 +3,6 @@ package ch.hsr.maloney.util;
 import ch.hsr.maloney.storage.Artifact;
 import ch.hsr.maloney.storage.FileAttributes;
 
-import java.util.Date;
-
 /**
  * File Category, used for generating reports.
  *
@@ -13,15 +11,13 @@ import java.util.Date;
 public interface Category {
     /**
      *
-     * @param fileAttributes    fileAttribute with fields which should be matched, other fields shall be null
-     * @return  True when specified parameter matched, false when not
+     * @return  Get parametrized object which files of this categories should match to. Irrelevant fields are null.
      */
-    boolean matchAttribute(FileAttributes fileAttributes);
+    FileAttributes matchAttribute();
 
     /**
      *
-     * @param artifact  artifact with fields which should be matched, other fields shall be null
-     * @return  True when specified parameter matched, false when not
+     * @return  Get parametrized object which files of this categories should match to. Irrelevant fields are null.
      */
-    boolean matchArtifact(Artifact artifact);
+    Artifact matchArtifact();
 }

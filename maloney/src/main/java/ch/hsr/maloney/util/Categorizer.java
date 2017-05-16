73624bfea519a3ca2e0ee7a04e6ba0a5f00785e7
Oliver Nietlispach <oliver.nietlispach@gmail.com>
2017-05-16 15:45:24 +0200

    Added methods for matching to Categorizer

--- maloney/src/main/java/ch/hsr/maloney/util/Categorizer.java
+++ maloney/src/main/java/ch/hsr/maloney/util/Categorizer.java
@@ -10,40 +10,57 @@ import java.util.List;
  */
 public class Categorizer {
     /**
-     *
-     * @param fileAttributes    File which will get categorized
-     * @param category          Category which file belongs to
-     * @return                  True if category matches, false if not
+     * @param fileAttr File which will get categorized
+     * @param category Category which file belongs to
+     * @return True if category matches, false if not
      */
-    boolean isMatch(FileAttributes fileAttributes, Category category){
-        //TODO this
-        FileAttributes targetFileAttributes = category.matchAttribute();
-        return matchFileName(fileAttributes, targetFileAttributes);
+    boolean isMatch(FileAttributes fileAttr, Category category) {
+        FileAttributes target = category.matchAttribute();
+        Artifact targetArtifact = category.matchArtifact();
+
+        return
+                matchFileName(fileAttr, target) &&
+                matchFilePath(fileAttr, target) &&
+                matchDateCreated(fileAttr, target) &&
+                matchDateChanged(fileAttr, target) &&
+                matchDateAccessed(fileAttr, target) &&
+                matchArtifacts(fileAttr.getArtifacts(), targetArtifact);
 
     }
 
-    private boolean matchFileName(FileAttributes fileAttributes, FileAttributes targetFileAttributes) {
-        return targetFileAttributes.getFileName() != null && targetFileAttributes.getFileName().equals(fileAttributes.getFileName());
+    private boolean matchFileName(FileAttributes fileAttr, FileAttributes target) {
+        return target.getFileName() == null || target.getFileName().equals(fileAttr.getFileName());
     }
 
-    private boolean matchFilePath(FileAttributes fileAttributes, FileAttributes targetFileAttributes) {
-        return targetFileAttributes.getFilePath() != null && targetFileAttributes.getFilePath().equals(fileAttributes.getFilePath());
+    private boolean matchFilePath(FileAttributes fileAttr, FileAttributes target) {
+        return target.getFilePath() == null || target.getFilePath().equals(fileAttr.getFilePath());
     }
 
-    private boolean matchDateChanged(FileAttributes fileAttributes, FileAttributes targetFileAttributes) {
-        return targetFileAttributes.getDateChanged() != null && targetFileAttributes.getDateChanged().equals(fileAttributes.getDateChanged());
+    private boolean matchDateChanged(FileAttributes fileAttr, FileAttributes target) {
+        return target.getDateChanged() == null || target.getDateChanged().equals(fileAttr.getDateChanged());
     }
 
-    private boolean matchDateCreated(FileAttributes fileAttributes, FileAttributes targetFileAttributes) {
-        return targetFileAttributes.getDateCreated() != null && targetFileAttributes.getDateCreated().equals(fileAttributes.getDateCreated());
+    private boolean matchDateCreated(FileAttributes fileAttr, FileAttributes target) {
+        return target.getDateCreated() == null || target.getDateCreated().equals(fileAttr.getDateCreated());
     }
 
-    private boolean matchDateAccessed(FileAttributes fileAttributes, FileAttributes targetFileAttributes) {
-        return targetFileAttributes.getDateAccessed() != null && targetFileAttributes.getDateAccessed().equals(fileAttributes.getDateAccessed());
+    private boolean matchDateAccessed(FileAttributes fileAttr, FileAttributes target) {
+        return target.getDateAccessed() == null || target.getDateAccessed().equals(fileAttr.getDateAccessed());
     }
 
-    private boolean matchArtifacts(List<Artifact> artifacts, Artifact targetArtifact){
-        //TODO this
-        return false;
+    private boolean matchArtifactOriginator(Artifact artifact, Artifact targetArtifact){
+        return targetArtifact.getOriginator() == null || targetArtifact.getOriginator().equals(artifact.getOriginator());
+    }
+
+    private boolean matchArtifactType(Artifact artifact, Artifact targetArtifact){
+        return targetArtifact.getType() == null || targetArtifact.getType().equals(artifact.getType());
+    }
+
+    private boolean matchArtifactValue(Artifact artifact, Artifact targetArtifact){
+        return targetArtifact.getValue() == null || targetArtifact.getValue().equals(artifact.getValue());
+    }
+
+    private boolean matchArtifacts(List<Artifact> artifacts, Artifact targetArtifact) {
+        return targetArtifact == null || artifacts.stream().filter(art -> matchArtifactOriginator(art, targetArtifact) && matchArtifactType(art, targetArtifact) && matchArtifactValue(art, targetArtifact)).count() > 0;
     }
 }
